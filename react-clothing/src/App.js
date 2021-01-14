fdbe941f378c49088f23cb541b98c8788f00cc7b
theisen92 <andrew.peter.theisen@gmail.com>
2021-01-13 22:19:25 -0600

    added header

--- react-clothing/src/App.js
+++ react-clothing/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
+import Header from "./components/header/header.com";
 import HomePage from "./pages/homepage/homepage.com";
 import ShopPage from "./pages/shop/shop.com";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
