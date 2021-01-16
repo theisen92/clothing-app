a8a65df015338c468b24184bfff21b45391b15da
theisen92 <andrew.peter.theisen@gmail.com>
2021-01-16 11:38:07 -0600

    sign-up page and styling

--- react-clothing/src/App.js
+++ react-clothing/src/App.js
@@ -3,8 +3,10 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/header/header.com";
+
 import HomePage from "./pages/homepage/homepage.com";
 import ShopPage from "./pages/shop/shop.com";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.com";
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
